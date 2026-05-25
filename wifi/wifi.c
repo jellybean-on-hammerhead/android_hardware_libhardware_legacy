16dfc613ebf1310f66d0bd123050c64f3edb82a5
scamdisk <158341159+scamdisk@users.noreply.github.com>
2026-05-25 09:50:58 +0300

    change the other reference because i forgot

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -477,7 +477,7 @@ int wifi_start_supplicant(int p2p_supported)
             __system_property_read(pi, NULL, supp_status);
             if (strcmp(supp_status, "running") == 0) {
                 return 0;
-            } else if (__system_property_serial(pi) != serial &&
+            } else if (pi->serial != serial &&
                     strcmp(supp_status, "stopped") == 0) {
                 return -1;
             }
